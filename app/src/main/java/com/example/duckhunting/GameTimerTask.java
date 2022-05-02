b580c7977b63adc323db552b3068622403acd374
Teriyurki <77750557+Teriyurki@users.noreply.github.com>
2022-05-01 19:27:07 -0500

    HW8DuckHunt

---
+++ app/src/main/java/com/example/duckhunting/GameTimerTask.java
@@ -0,0 +1,30 @@
+package com.example.duckhunting;
+
+import java.util.TimerTask;
+
+public class GameTimerTask extends TimerTask {
+    private Game game;
+    private GameView gameView;
+
+    public GameTimerTask(GameView view) {
+        gameView = view;
+        game = view.getGame();
+        game.startDuckFromRightTopHalf();
+    }
+
+    public void run() {
+        game.moveDuck();
+        if (game.bulletOffScreen())
+            game.loadBullet();
+        else if (game.isBulletFired())
+            game.moveBullet();
+        if (game.duckOffScreen()){
+            game.setDuckShot(false);
+        game.startDuckFromRightTopHalf();
+    } else if (game.duckHit()) {
+        game.setDuckShot(true);
+        ((MainActivity) gameView.getContext()).playHitSound();
+    }
+        gameView.postInvalidate();
+    }
+}
